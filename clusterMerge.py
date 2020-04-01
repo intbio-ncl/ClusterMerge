596afaa6221749d7fc7d6b31b7d7c9d1b82f08a0
NeilWipat <anil.wipat@ncl.ac.uk>
2020-04-01 19:42:03 +0100

    initial shell

--- clusterMerge.py
+++ clusterMerge.py
@@ -7,3 +7,19 @@ import time
 import requests
 from neo4j import GraphDatabase
 import networkx as nx
+
+#Read in a cluster exported from cytoscape as a graphml file to produce Graph P
+
+#Extract the gene IDs from the cluster as gene names in the gene list
+
+#Query the repotrialDB to produce graphml Graph R
+	#Extract the genes, the proteins encoded by the genes
+	#Extract the drugs_has_target for those proteins
+	#Extract the disorders associated with a gene
+
+#Merge R and P on gene name
+
+#Write out the merged graph as a file
+
+
+
